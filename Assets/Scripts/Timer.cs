e252f29515412ce6057a780988620bd078751488
domothebomo <dfanaris@gmail.com>
2023-12-05 20:56:57 -0800

    added timer to UI, and added very basic items + started infrastructure for spawn + score managers

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,13 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class Timer : MonoBehaviour
 {
+    public GameObject TimerText;
+    public float TimeLimit = 60.0f;
+
     private float timeSinceStart = 0;
     private void Update()
     {
         timeSinceStart += Time.deltaTime;
+        TimerText.GetComponent<TMP_Text>().text = "Time to Departure: " + Mathf.Ceil(TimeLimit - timeSinceStart).ToString();
     }
     private void OnDestroy()
     {
